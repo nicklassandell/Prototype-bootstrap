b3a26ec384ef31a446b163a1eb1e60b409fac423
Nicklas Sandell <nike.sandell@gmail.com>
2014-06-30 17:43:53 +0200

    Initial commit

---
+++ grunt/gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Compass SCSS
+		 */
+		 sass: {
+		 	dist: {
+		 		options: {
+			 		output: 'compressed',
+			 		sourceComments: 'map',
+			 		sourceMap: 'sass'
+			 	},
+		 		files: {
+		 			'../css/style.css': '../scss/style.scss'
+		 		}
+		 	},
+		 	dev: {
+		 		options: {
+			 		output: 'expanded'
+			 	},
+		 		files: {
+		 			'../css/style.css': '../scss/style.scss'
+		 		}
+		 	},
+		 },
+
+
+		/**
+		 * Jade
+		 */
+		jade: {
+			dist: {
+				options: {
+					pretty: true,
+				},
+				files: {
+					'../index.html' : ['../jade/index.jade']
+				}
+			}
+		},
+
+		emberTemplates: {
+			dist: {
+				compile: {
+					amd: true,
+					templateBasePath: '../js/templates/' // Not working?
+				},
+				files: {
+					'../js/templates/compiled.js': '../js/templates/*.hbars'
+				}
+			}
+		},
+
+
+		/**
+		 * File watcher
+		 */
+		watch: {
+			css: {
+				files: ['../scss/*.scss'],
+				tasks: ['sass:dev']
+			},
+			jade: {
+				files: ['../jade/index.jade'],
+				tasks: ['jade:dist']
+			}
+		}
+	});
+
+
+
+	/**
+	 * Load modules that we'll use
+	 */
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+
+	grunt.loadNpmTasks('grunt-ember-templates');
+};
